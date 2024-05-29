2f9d5aad5117cb2a5276223b90dca3d91e37db2a
alyonafilyaeva <alyonchikfilyaeva@yandex.ru>
2024-05-29 11:54:42 +0500

    add validate

--- src/components/addBookPage.jsx
+++ src/components/addBookPage.jsx
@@ -6,20 +6,24 @@ import { collection, getFirestore, addDoc } from "firebase/firestore";
 
 function AddBookPage() {
   const db = getFirestore();
-  const nav = useNavigate()
+  const nav = useNavigate();
+  const date = new Date()
   const addBook = async (values) => {
+    if (!values.title || !values.authors || values.createdAt <=1800 || values.rating <0 || values.rating > 10) {
+      alert("Проверьте корректность введенных данных");
+    } else {
+      const data = await addDoc(collection(db, "books"), {
+        ISBN: values.ISBN,
+        authors: values.authors.split(", "),
+        createdAt: +values.createdAt,
+        rating: +values.rating,
+        title: values.title,
+      }).then(() => {
+        nav("/");
+      });
+    }
 
-    console.log(values)
-    const data = await addDoc(collection(db, "books"), {
-      ISBN: values.ISBN,
-      authors: values.authors.split(', '),
-      createdAt: +values.createdAt,
-      rating: +values.rating,
-      title: values.title,
-    }).then(() => {
-        nav('/')
-    })
-    console.log('отправилсь')
+    console.log("отправилсь");
   };
 
   return (
@@ -34,7 +38,7 @@ function AddBookPage() {
         }}
         onSubmit={(values) => addBook(values)}
       >
-        {({ handleChange,handleSubmit, values }) => (
+        {({ handleChange, handleSubmit, values }) => (
           <form className="form">
             <div className="input_item">
               <p>Название книги</p>
@@ -44,49 +48,60 @@ function AddBookPage() {
                 /* onChange={handleChange('title')} */
                 value={values.title}
                 placeholder="Название книги"
+                maxLength={100}
+                required
               />
             </div>
             <div className="input_item">
               <p>Автор</p>
               <Field
-              name="authors"
+                name="authors"
                 className="input"
                 onChange={handleChange}
                 value={values.authors}
                 placeholder="Если авторов несколько, введите их через запятую"
+                required
               />
             </div>
             <div className="input_item">
               <p>Год публикации</p>
               <Field
-              name="createdAt"
+                name="createdAt"
                 className="input"
                 onChange={handleChange}
                 value={values.createdAt}
                 placeholder="Год публикации"
+                type="number"
               />
+              {(values.createdAt <1800 || values.createdAt > date.getFullYear()) && <p style={{color: "#F21F39", fontSize: "14px"}}>Год публикации должен быть не раньше 1800</p>}
             </div>
             <div className="input_item">
               <p>Рейтинг</p>
               <Field
-              name="rating"
+                name="rating"
                 className="input"
                 onChange={handleChange}
                 value={values.rating}
                 placeholder="Рейтинг"
+                type="number"
+                pattern="[0-9]*"
               />
+              {(values.rating <0 || values.rating > 10) && <p style={{color: "#F21F39", fontSize: "14px"}}>Рейтинг должен быть от 0 до 10</p>}
             </div>
             <div className="input_item">
               <p>ISBN</p>
               <Field
-              name="ISBN"
+                name="ISBN"
                 className="input"
                 onChange={handleChange}
                 value={values.ISBN}
                 placeholder="ISBN"
               />
             </div>
-            <button type="submit" onClick={handleSubmit}> Добавить книгу</button>
+            <button type="submit" onClick={handleSubmit}>
+              {" "}
+              Добавить книгу
+            </button>
           </form>
         )}
       </Formik>
